57f1824cb97baa1045eb79705ec9bbf85120781c
Anderson Rocha <andersonecomp091@gmail.com>
2020-01-19 19:56:20 -0300

    Criação dos componentes de formulario e lista para cadastro e listagem das tarefas

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import PageHeader from '../template/PageHeader';
-// import { Container } from './styles';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
 
 export default class Todo extends Component {
   render() {
     return (
         <div>
             <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+            <TodoForm/>
+            <TodoList/>
         </div>
     );
   }
---
+++ src/todo/TodoList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default props =>(
+    <div>
+        <h1>List</h1>
+    </div>
+);
