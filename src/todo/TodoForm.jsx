c9eaf058dcf0e1ca7021b7bbe92c07d45e22de7d
Anderson Rocha <andersonecomp091@gmail.com>
2020-01-19 20:24:53 -0300

    Criacao de primeiros campos do formulario

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export default props=>(
-    <div>
-        <h1>Form</h1>
+export default props => (
+    <div role="form" className="todoForm">
+        <div className="input-group">
+            <div className="col-xs-12 col-sm-9 col-md-10">
+                <input type="text" className="form-control" placeholder="Adicione uma tarefa" />
+            </div>
+            <span className="input-group-btn">
+                <div className="col-xs-12 col-sm-3 col-md-2">
+                    <button className="btn btn-primary">
+                        <i className="fa fa-plus"></i>
+                    </button>
+                </div>
+            </span>
+        </div>
     </div>
 );
