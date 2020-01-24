77238230e67c35a4699a741b027c67ba882cd44d
Anderson Rocha <andersonecomp091@gmail.com>
2020-01-23 21:52:08 -0300

    Componentizacao de itens para reuso

---
+++ src/template/IconButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import If from '../template/If';
+
+export default props => (
+    
+    <If test={!props.hide}>
+        < button className={'btn btn-' + props.style}
+            onClick={props.onClick}
+        >
+            <i className={'fa fa-' + props.icon}></i>
+        </button >
+    </If>
+)
+
