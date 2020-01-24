5931853c687960401b7a24c071d68b741902aa8a
Anderson Rocha <andersonecomp091@gmail.com>
2020-01-23 21:52:42 -0300

    Aplicacao de componentes customizaveis formulario

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import Grid from '../template/Grid';
+import IconButton from '../template/IconButton';
 
 export default props => (
     <div role="form" className="todoForm">
         <div className="input-group">
-            <div className="col-xs-12 col-sm-9 col-md-10">
+            <Grid cols='12 9 10'>
                 <input type="text" className="form-control" placeholder="Adicione uma tarefa" />
-            </div>
-            <span className="input-group-btn">
-                <div className="col-xs-12 col-sm-3 col-md-2">
-                    <button className="btn btn-primary">
-                        <i className="fa fa-plus"></i>
-                    </button>
-                </div>
-            </span>
+            </Grid>
+
+            <Grid cols="12 3 2">
+                <span className="input-group-btn">
+                    <IconButton  style="primary" icon="plus"></IconButton>
+                </span>
+            </Grid>
         </div>
     </div>
 );
