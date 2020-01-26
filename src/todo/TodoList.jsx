eea7d1ea32c305bd2032d6585d8089f2cc03f6bd
Anderson Rocha <andersonecomp091@gmail.com>
2020-01-26 13:15:10 -0300

    inclusão de grid exibindo itens e permitindo  edição

--- src/todo/TodoList.jsx
+++ src/todo/TodoList.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import IconButton from '../template/IconButton'
 // import { Container } from './styles';
 
-export default props =>(
-    <div>
-        <h1>List</h1>
-    </div>
-);
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || [];
+        return (
+            list.map(todo => (
+                <tr key={todo._id}>
+                    <td className={todo.done?'markedAsDone':''}>{todo.description}</td>
+                    <td>
+                        <IconButton style='success' icon='check'
+                            onClick={() => props.handleMarkAsDone(todo)}
+                            hide={todo.done} />
+                        <IconButton style='warning' icon='undo'
+                            onClick={() => props.handleMarkAsPending(todo)}
+                            hide={!todo.done} />
+                        <IconButton style='danger' icon='trash-o'
+                            onClick={() => props.handleRemove(todo)}
+                            hide={!todo.done}/>
+                    </td>
+                </tr>
+            ))
+        )
+    }
+
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    );
+};
