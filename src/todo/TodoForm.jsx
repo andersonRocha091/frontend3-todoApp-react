73ef43b25621794400131a280282ad97f4f5739c
Anderson Rocha <andersonecomp091@gmail.com>
2020-01-28 23:29:20 -0300

    Adicionado alguns atalhos para pesquisa, insercao e limpeza de campos

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -2,26 +2,39 @@ import React from 'react';
 import Grid from '../template/Grid';
 import IconButton from '../template/IconButton';
 
-export default props => (
-    <div role="form" className="todoForm">
-        <div className="input-group">
-            <Grid cols='12 9 10'>
-                <input type="text" className="form-control"
-                    placeholder="Adicione uma tarefa"
-                    onChange={props.handleChange}
-                    value={props.description}
-                />
-            </Grid>
+export default props => {
 
-            <Grid cols="12 3 2">
+    const keyHandler = (e) => {
+        if (e.key == 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        }
+        else if(e.key=='Escape'){
+            props.handleClear()
+        }
+    }
+
+    return (
+        <div role="form" className="todoForm">
+            <div className="input-group">
+                <Grid cols='12 9 10'>
+                    <input type="text" className="form-control"
+                        placeholder="Adicione uma tarefa"
+                        onChange={props.handleChange}
+                        onKeyUp={keyHandler}
+                        value={props.description}
+                    />
+                </Grid>
+
+                <Grid cols="12 3 2">
                     <IconButton style="primary" icon="plus"
                         onClick={props.handleAdd}
                     ></IconButton>
                     <IconButton style='info' icon='search'
-                    onClick={props.handleSearch}/>
+                        onClick={props.handleSearch} />
                     <IconButton style='default' icon='close'
-                        onClick={props.handleClear}/>
-            </Grid>
+                        onClick={props.handleClear} />
+                </Grid>
+            </div>
         </div>
-    </div>
-);
+    );
+}
